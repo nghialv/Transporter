2094975014f0c4dfa2dc84ab0c1568ec38ebf103
nghialv <levan_nghia@cyberagent.co.jp>
2015-03-27 17:12:58 +0900

    refactoring

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -22,58 +22,64 @@ class ViewController: UIViewController {
         let uploadUrl = "http://httpbin.org/post"
         
         // Downloading tasks
-        let task1 = DownloadTask(url: downloadUrl1).progress { cur, total in
+        let task1 = DownloadTask(url: downloadUrl1)
+            .progress { cur, total in
                 let per = Double(cur) / Double(total)
                 println("task1: downloading: \(per)")
             }
             .completed {
-                NSLog("task1: completed")
+                println("task1: completed")
             }
         
-        let task2 = DownloadTask(url: downloadUrl2).progress { cur, total in
-            let per = Double(cur) / Double(total)
-            println("task2: downloading: \(per)")
+        let task2 = DownloadTask(url: downloadUrl2)
+            .progress { cur, total in
+                let per = Double(cur) / Double(total)
+                println("task2: downloading: \(per)")
             }
             .completed {
-                NSLog("task2: completed")
-        }
+                println("task2: completed")
+            }
        
-        let task3 = DownloadTask(url: downloadUrl3).progress { cur, total in
-            let per = Double(cur) / Double(total)
-            println("task3: downloading: \(per)")
+        let task3 = DownloadTask(url: downloadUrl3)
+            .progress { cur, total in
+                let per = Double(cur) / Double(total)
+                println("task3: downloading: \(per)")
             }
             .completed {
-                NSLog("task3: completed")
-        }
+                println("task3: completed")
+            }
         
         // Uploading tasks
         let path = NSBundle.mainBundle().pathForResource("zip_file", ofType: "zip")
         let fileUrl = NSURL(fileURLWithPath: path!)!
-        let task4 = UploadTask(url: uploadUrl, file: fileUrl).progress { cur, total in
-            let per = Double(cur) / Double(total)
-            println("task4: uploading: \(per)")
+        let task4 = UploadTask(url: uploadUrl, file: fileUrl)
+            .progress { cur, total in
+                let per = Double(cur) / Double(total)
+                println("task4: uploading: \(per)")
             }
             .completed {
-                NSLog("task4: completed")
-        }
+                println("task4: completed")
+            }
         
         let path2 = NSBundle.mainBundle().pathForResource("zip_file2", ofType: "zip")
         let fileUrl2 = NSURL(fileURLWithPath: path!)!
-        let task5 = UploadTask(url: uploadUrl, file: fileUrl2).progress { cur, total in
-            let per = Double(cur) / Double(total)
-            println("task5: uploading: \(per)")
+        let task5 = UploadTask(url: uploadUrl, file: fileUrl2)
+            .progress { cur, total in
+                let per = Double(cur) / Double(total)
+                println("task5: uploading: \(per)")
             }
             .completed {
-                NSLog("task5: completed")
-        }
+                println("task5: completed")
+            }
         
         let tasks = (6...8).map { i -> UploadTask in
-            let task = UploadTask(url: uploadUrl, file: fileUrl).progress { cur, total in
+            let task = UploadTask(url: uploadUrl, file: fileUrl)
+                .progress { cur, total in
                     let per = Double(cur) / Double(total)
                     println("task\(i): uploading: \(per)")
                 }
                 .completed {
-                    NSLog("task\(i): completed")
+                    println("task\(i): completed")
                 }
             return task
         }
@@ -81,40 +87,40 @@ class ViewController: UIViewController {
         /*
         Transporter.push([task1, task2])
             .completed {
-                NSLog("transaction1: completed")
+                println("transaction1: completed")
             }
             .push(task3)
             .completed {
-                NSLog("transaction2: completed")
+                println("transaction2: completed")
             }
             .resume()
         
         Transporter.push(task4)
             .completed {
-                NSLog("transaction3: completed")
+                println("transaction3: completed")
             }
             .resume()
         */
         
-        Transporter.push(task4 <--> task5)
+        Transporter.push(task1 <--> task2)
             .progress { cur, total in
                 let ratio = Double(cur) / Double(total)
-                NSLog("transaction4: \(ratio))")
+                println("transaction4: \(ratio)")
             }
             .completed {
-                NSLog("transaction4: completed")
+                println("transaction4: completed")
             }
             .resume()
         
         /*
             .push(task1)
             .completed {
-                NSLog("transaction5: completed")
+                println("transaction5: completed")
             }
             .resume()
             .push(tasks[2] --> tasks[3] --> tasks[4])
             .completed {
-                NSLog("transaction5: completed")
+                println("transaction5: completed")
             }
             .resume()
             */
